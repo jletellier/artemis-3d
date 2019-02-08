404b85f2d1eca352b3c5c46de99b52d678bc80a5
Julien Letellier <letellier.julien@gmail.com>
2019-02-08 11:39:08 +0100

    Add material icons

--- src/client/components/App.ts
+++ src/client/components/App.ts
@@ -1,14 +1,48 @@
-import { LitElement, html, customElement } from 'lit-element';
+import { LitElement, html, css, customElement } from 'lit-element';
+import '@material/mwc-icon';
+import '@material/mwc-button';
 import './BabylonRenderer';
 
 @customElement('smaat-app')
 export default class App extends LitElement {
 
+    static styles = css`
+        div {
+            padding: 5px;
+        }
+        .navbar {
+            background-color: lightgray;
+        }
+    `;
+
     render() {
         return html`
-            <h1>Hi</h1>
+            <div class="navbar">
+                <mwc-button icon="create_new_folder" label="New project" raised
+                    @click="${this.handleNewProject}">
+                </mwc-button>
+                <mwc-button icon="add" label="Add marker" raised
+                    @click="${this.handleAddMarker}">
+                </mwc-button>
+                <mwc-button icon="share" label="Share project" raised
+                    @click="${this.handleShareProject}">
+                </mwc-button>
+            </div>
+            
             <smaat-babylon-renderer></smaat-babylon-renderer>
         `;
+    }
+
+    handleNewProject() {
+        console.log('new project');
+    }
+
+    handleAddMarker() {
+        console.log('add marker');
+    }
+
+    handleShareProject() {
+        console.log('share project');
     }
 
 }
