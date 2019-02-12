86a21bc4c98d5733ffd40eafd80c2396565ebb88
Julien Letellier <letellier.julien@gmail.com>
2019-02-12 11:31:55 +0100

    Add scene serialization

--- src/client/data/project.ts
+++ src/client/data/project.ts
@@ -1,10 +1,10 @@
-import { Scene, Mesh, StandardMaterial, Texture, TransformNode } from 'babylonjs';
+import { Scene, Mesh, StandardMaterial, Texture, TransformNode, SceneSerializer } from 'babylonjs';
 
 class Project {
     
     _id: string = null;
-
-    scene: Scene;
+    _scene: Scene;
+    _markerContainer: TransformNode;
 
     set id(newID: string) {
         if (this._id !== newID) {
@@ -16,26 +16,34 @@ class Project {
         return this._id;
     }
 
+    set scene(newScene: Scene) {
+        this._scene = newScene;
+        this._markerContainer = this._scene.getTransformNodeByName('MarkerContainer');
+        if (!this._markerContainer) {
+            this._markerContainer = new TransformNode('MarkerContainer', this._scene);
+        }
+    }
+
     addMarker(file: File) {
-        let markerContainer = this.scene.getTransformNodeByName('MarkerContainer');
-        if (!markerContainer) {
-            markerContainer = new TransformNode('MarkerContainer', this.scene);
+        if (!this._markerContainer) {
+            return;
         }
 
-        const plane = Mesh.CreatePlane(file.name, 20.0, this.scene);
-        plane.setParent(markerContainer);
-        const material = new StandardMaterial('MarkerMaterial', this.scene);
-        const texture = new Texture(window.URL.createObjectURL(file), this.scene);
+        const plane = Mesh.CreatePlane(file.name, 20.0, this._scene);
+        plane.setParent(this._markerContainer);
+        const material = new StandardMaterial('MarkerMaterial', this._scene);
+        const texture = new Texture(window.URL.createObjectURL(file), this._scene);
         material.diffuseTexture = texture;
         material.diffuseTexture.hasAlpha = true;
         material.backFaceCulling = false;
         plane.material = material;
+
+        this._saveToServer();
     }
 
     getMarkerNames() {
-        const markerContainer = this.scene.getTransformNodeByName('MarkerContainer');
-        if (markerContainer) {
-            const nodes = markerContainer.getChildTransformNodes(true);
+        if (this._markerContainer) {
+            const nodes = this._markerContainer.getChildTransformNodes(true);
             return nodes.map(value => value.name);
         }
         return [];
@@ -43,6 +51,11 @@ class Project {
 
     _loadFromServer() {
         console.log('load new content');
+    }
+
+    _saveToServer() {
+        const serializedScene = SceneSerializer.Serialize(this._scene);
+        console.log(JSON.stringify(serializedScene));
     }
 
 }
