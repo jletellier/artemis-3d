cdcd1617e252ecaf3f006524dbfbc53c06d57d83
Julien Letellier <letellier.julien@gmail.com>
2019-08-26 17:00:42 +0200

    Reintroduce server module

---
+++ server/src/server.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+import fs from 'fs';
+import https from 'https';
+import express from 'express';
+import ws from 'ws';
+import nodeWatch from 'node-watch';
+
+const rootPath = path.resolve(__dirname, '../../');
+const port = 8443;
+const credentials = {
+    key: fs.readFileSync(
+        path.resolve(rootPath, 'dev-cert', 'jl-l-01.local+4-key.pem'),
+        'utf8',
+    ),
+    cert: fs.readFileSync(
+        path.resolve(rootPath, 'dev-cert', 'jl-l-01.local+4.pem'),
+        'utf8',
+    ),
+};
+
+const app = express();
+const httpsServer = https.createServer(credentials, app);
+const wss = new ws.Server({ server: httpsServer });
+
+nodeWatch(path.resolve(rootPath, 'examples'), { recursive: true }, reloadClient);
+nodeWatch(path.resolve(rootPath, 'viewer/dist/index.js'), {}, reloadClient);
+
+function reloadClient() {
+    console.log('File changed...');
+}
+
+app.use('/', express.static(path.resolve(rootPath, 'examples')));
+app.use('/dist/', express.static(path.resolve(rootPath, 'viewer/dist')));
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(rootPath, 'examples', 'index.html'));
+});
+
+httpsServer.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
