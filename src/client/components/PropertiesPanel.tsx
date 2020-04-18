bc2a444de2cb72ef18238616c54429e1b89be10e
AndSelm <and.selm+git@gmail.com>
2020-04-18 19:56:27 +0200

    FIX: giving component views a kinda unique id based on nodeId due to bug in react-jsonform-schema

--- src/client/components/PropertiesPanel.tsx
+++ src/client/components/PropertiesPanel.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { FunctionComponent } from 'react';
 
 import { useProjectState, changeProjectState } from '../stores/projectStore';
-import { TransformPropertyGroup } from './TransformPropertyGroup';
 import { useUserState } from '../stores/userStore';
 import { transformView } from './objectViews/TransformView';
 
@@ -24,18 +23,25 @@ const PropertiesPanel: FunctionComponent = () => {
 
       const subpanels = views
         .filter((view) => view.isRelevant(node)) // TODO: filter by visibility / collapsed state
-        .map((view) => (
-          <view.component
-            key={view.name}
-            focus={view.lens.get(node)}
-            onUpdate={(focus) => {
-              changeProjectState((state) => {
-                const currNode = state.gltf.nodes[nodeId]; // @Julien: is this different than the node above?
-                view.lens.set(currNode, focus);
-              });
-            }}
-          />
-        ));
+        .map((view) => {
+          const onUpdate = (focus: any) => {
+            changeProjectState((state) => {
+              const currNode = state.gltf.nodes[nodeId];
+              view.lens.set(currNode, focus);
+            });
+          };
+          return (
+            <view.component
+              // TODO: possible performance issue
+              // if nodeId is not part of key, onUpdate may have old data (wrong nodeId) due to
+              // a bug in react-jsonschema-form
+              // https://github.com/rjsf-team/react-jsonschema-form/issues/1708
+              key={`${nodeId}-${view.name}`}
+              focus={view.lens.get(node)}
+              onUpdate={onUpdate}
+            />
+          );
+        });
 
       return <div>{subpanels}</div>;
 
